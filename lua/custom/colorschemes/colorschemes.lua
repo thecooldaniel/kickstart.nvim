3ef8a5c35d0d2fa0a5917782636053fa66b18811
Daniel Owens <kyoji1@gmail.com>
2024-05-20 22:27:23 -0400

    update

--- lua/custom/colorschemes/colorschemes.lua
+++ lua/custom/colorschemes/colorschemes.lua
@@ -53,7 +53,7 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'monokai-pro'
+      vim.cmd.colorscheme 'monokai-pro-spectrum'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
